7086f3e5f912b6bdac94466bf7532b9728a332ba
aleksey.krasnov <krasnov.80@mail.ru>
2023-07-13 14:07:53 +0300

    Set and Class

--- ClasCat.java
+++ ClasCat.java
@@ -1,61 +1,41 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+public class Cat {
+    int numPassport;
+    String owner;
+    String name;
+    int age;
+    String color;
 
-public class ClasCat {
-    /*
-    Продумайте структуру класса Кот. Какие поля и методы будут актуальны для приложения, которое является
-    а) информационной системой ветеринарной клиники
-    б) архивом выставки котов
-    в) информационной системой Театра кошек Ю. Д. Куклачёва
-    Можно записать в текстовом виде, не обязательно реализовывать в java.
+    public Cat(int numPassport, String name, String owner, int age, String color) {
+        this.numPassport = numPassport;
+        this.owner = owner;
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
 
-    1. Реализуйте 1 из вариантов класса Cat из предыдущего задания, можно использовать не все придуманные поля и методы.
-    Создайте несколько экземпляров этого класса, выведите их в консоль.
-    2. Добейтесь того, чтобы при выводе в консоль объекта типа Cat, выводилась его кличка, цвет и возраст
-    (или другие параметры на ваше усмотрение).
-     */
-    /*
-    1. Создайте множество, в котором будут храниться экземпляры класса Cat - HashSet<Cat>.
-    Поместите в него некоторое количество объектов.
-    2. Создайте 2 или более котов с одинаковыми параметрами в полях. Поместите их во множество.
-    Убедитесь, что все они сохранились во множество.
-    3. Создайте метод public boolean equals(Object o)
-    Пропишите в нём логику сравнения котов по параметрам, хранимым в полях.
-    То есть, метод должен возвращать true, только если значения во всех полях сравниваемых объектов равны.
-    4. Выведите снова содержимое множества из пункта 2, убедитесь, что дубликаты удалились.
+    @Override
+    public String toString() {
+        return  "numPassport:" + numPassport +
+                ", owner:" + owner +
+                ", name:" + name +
+                ", age:" + age +
+                ", color:" + color;
+    }
 
-     */
+    @Override
+    public int hashCode() {
+        return name.hashCode() + 7*numPassport + 13*owner.hashCode();
+    }
 
-    public static void main(String[] args) {
-        Set<Cat> set = new HashSet<>();
-
-        Cat cat1 = new Cat(121, "Васька", "Иван Петрович",8,"черный");
-        Cat cat2 = new Cat(22, "Мурка", "Мария Ивановна",9,"рыжий");
-        Cat cat3 = new Cat(564, "Барсик", "Иван Васильевич",8,"белый");
-        Cat cat4 = new Cat(564, "Барсик", "Иван Васильевич",8,"белый");
-
-        set.add(cat1);
-        set.add(cat2);
-        set.add(cat3);
-        set.add(cat4);
-
-//        System.out.println(cat3.equals(cat4));
-
-//        for (Cat cat: set){
-//            System.out.println(cat);
-//        }
-
-        List<Cat> res = new ArrayList<>();
-        for (Cat cat: set){
-            if (cat.age == 8){
-                res.add(cat);
-            }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
         }
-
-        for (Cat cat: res){
-            System.out.println(cat);
+        if (!(obj instanceof Cat)){
+            return false;
         }
+        Cat cat = (Cat) obj;
+        return numPassport == cat.numPassport && name.equalsIgnoreCase(cat.name) && owner.equalsIgnoreCase(cat.owner);
     }
 }
